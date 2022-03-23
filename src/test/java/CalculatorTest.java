03844ebb879b4d2769004082a20cd48cff32c836
susmitha2027 <102210744+susmitha2027@users.noreply.github.com>
2022-03-23 14:00:57 +0530

    Update CalculatorTest.java

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -6,6 +6,6 @@ public class CalculatorTest {
     @Test
     public void testFindMax(){
         assertEquals(4,Calculator.findMax(new int[]{1,3,4,2}));
-        assertNotEquals(-1,Calculator.findMax(new int[]{-12,-1,-3,-4,-2}));
+        assertEquals(-1,Calculator.findMax(new int[]{-12,-1,-3,-4,-2}));
     }
 }
